4570dc3ae63aefb2c69602153ed14b026bda0f7a
Marcelo Oliveira <capyvara@gmail.com>
2017-06-23 11:36:01 -0300

    Tests for multi arch MachO

--- lib/fastlane/plugin/store_sizer/helper/store_sizer_helper.rb
+++ lib/fastlane/plugin/store_sizer/helper/store_sizer_helper.rb
@@ -24,10 +24,6 @@ module Fastlane
         FastlaneCore::CommandExecutor.execute(command: command, print_command: false, print_all: false)
       end
 
-      def self.align_next(offset, align)
-        return (offset + (align - 1)) - ((offset + (align - 1)) % align)
-      end
-
       def self.encryption_segments(binary_path)
         require 'macho'
 
@@ -36,17 +32,20 @@ module Fastlane
         file = MachO.open(binary_path)
         if file.kind_of?(MachO::FatFile)
           file.fat_archs.each_index do |arch_index|
-            segments.push(macho_encryption_segment(file.machos[arch_index], file.fat_archs[arch_index].offset))
+            segment = macho_encryption_segment(file.machos[arch_index], file.fat_archs[arch_index].offset)
+            segments.push(segment) unless segment.nil?
           end
         elsif file.kind_of?(MachO::MachOFile)
-          segments.push(macho_encryption_segment(file, 0))
+          segment = macho_encryption_segment(file, 0)
+          segments.push(segment) unless segment.nil?
         end
-        return segments
+        segments
       end
 
       def self.macho_encryption_segment(macho, file_offset)
         encryption_info = (macho.magic32? ? macho[:LC_ENCRYPTION_INFO] : macho[:LC_ENCRYPTION_INFO_64]).first
-        return [file_offset + encryption_info.cryptoff, encryption_info.cryptsize]
+        return nil if encryption_info.nil?
+        [file_offset + encryption_info.cryptoff, encryption_info.cryptsize]
       end
     end
   end
